dea2024f2da7c9c72cda900696bce07aa72b14fc
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-07 01:15:59 +0530

    icon change

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        {/* <a href="https://www.instagram.com/" className="home__social-icon" >
+        <a href="https://www.instagram.com/" className="home__social-icon" rel="noreferrer" target="_blank">
             <i class="uil uil-instagram"></i>
         </a>
-        <a href="" className="home__social-icon" >
+        <a href=" " className="home__social-icon"  target="_blank">
             <i class="uil uil-dribbble"></i>
         </a>
-        <a href="https://github.com/" className="home__social-icon" >
+        <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i class="uil uil-github-alt"></i>
-        </a> */}
+        </a>
     </div>
   )
 }
