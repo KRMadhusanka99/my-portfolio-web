246ac169190c562f62704fb3426c06c51bb16931
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-07 04:27:45 +0530

    add home session for web

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,15 +3,22 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/" className="home__social-icon" rel="noreferrer" target="_blank">
-            <i class="uil uil-instagram"></i>
-        </a>
-        <a href=" " className="home__social-icon"  target="_blank">
-            <i class="uil uil-dribbble"></i>
-        </a>
         <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i class="uil uil-github-alt"></i>
         </a>
+        <a href="https://linkedin.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-linkedin-alt"></i>
+        </a>
+        <a href="https://medium.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-medium-m"></i>
+        </a>
+        <a href="https://www.instagram.com/" className="home__social-icon" rel="noreferrer" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/" className="home__social-icon"  rel="noreferrer" target="_blank">
+            <i class="uil uil-facebook-f"></i>
+        </a>
+        
     </div>
   )
 }
