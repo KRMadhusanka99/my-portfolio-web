7c3dbb0a061a64795d76c0bfa66fdd30a744b7b7
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-07 20:44:55 +0530

    add skills section

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Backend Development</h3>
+        <div className="skills__box">
+            <div className="skills__groups">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">PHP</h3>
+                        <span className="skills__level">Intermidiate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Java</h3>
+                        <span className="skills__level">Advance</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Python</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+            </div>
+            <div className="skills__groups">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Springboot</h3>
+                        <span className="skills__level">Intermidiate</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Nodejs</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Sql</h3>
+                        <span className="skills__level">Intermidiate</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Backend
---
+++ src/components/skills/Mobile.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Mobile = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Mobile Development</h3>
+        <div className="skills__box">
+            <div className="skills__groups">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Java</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Objective C</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Kotlin</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+            </div>
+            <div className="skills__groups">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Flutter</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Swift</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">React Native</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Mobile
