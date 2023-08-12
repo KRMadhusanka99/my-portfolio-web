c1033770b7092cd6987ea44c793cc33078ea5471
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-13 03:15:56 +0530

    add projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,8 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work1 from "../../assets/work1.png";
+import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
 
 export const projectsData = [
     {
@@ -17,42 +17,42 @@ export const projectsData = [
         image: Work2,
         title: "Basic Calculator",
         category: "mobile",
-        link: "https://github.com/KRMadhusanka99/SENG_22212_Ecommerce_Web_Project.git",
+        link: "https://github.com/KRMadhusanka99/Basic_Mobile_Calculator_App.git",
       },
       {
         id: 3,
         image: Work3,
         title: "Snake Game",
         category: "game",
-        link: "https://github.com/KRMadhusanka99/SENG_22212_Ecommerce_Web_Project.git",
+        link: "https://github.com/KRMadhusanka99/Simple_Snake_Game.git",
       },
       {
         id: 4,
         image: Work4,
         title: "My Portfolio website",
         category: "web",
-        link: "https://github.com/KRMadhusanka99/SENG_22212_Ecommerce_Web_Project.git",
+        link: "https://github.com/KRMadhusanka99/my-portfolio-web.git",
       },
       {
         id: 5,
         image: Work5,
         title: "Basic php website",
         category: "web",
-        link: "https://github.com/KRMadhusanka99/SENG_22212_Ecommerce_Web_Project.git",
+        link: "https://github.com/KRMadhusanka99/SENG_21253_Final_Web_Project.git",
       }
 ];
 
 export const projectsNav = [
     {
-        name: 'all',
+        name: 'All',
     },
     {
-        name: 'web',
+        name: 'Web',
     },
     {
-        name: 'mobile',
+        name: 'Mobile',
     },
     {
-        name: 'game',
+        name: 'Game',
     },
 ] 
